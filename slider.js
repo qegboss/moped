d9e97499494f82a47972f822907c1c3d3af7e199
qegboss <glovno666@gmail.com>
2026-08-13 14:38:57 +0300

    first commit

---
+++ slider.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    speed: 700,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+})
