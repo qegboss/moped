d2edb86b1a924b7b5e80aa0cbc872545c860bb1c
qegboss <glovno666@gmail.com>
2026-09-22 19:24:16 +0300

    swiper2

--- slider.js
+++ slider.js
@@ -10,3 +10,22 @@ const swiper = new Swiper(".swiper", {
         prevEl: ".swiper-button-prev"
     }
 })
+
+const swiper2 = new Swiper(".slider-2", {
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 1.35,
+    spaceBetween: 8,
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 16
+        }
+    },
+    initialSlide: 1,
+    speed: 700,
+    navigation: {
+        nextEl: ".slider-2-button-next",
+        prevEl: ".slider-2-button-prev"
+    }
+})
